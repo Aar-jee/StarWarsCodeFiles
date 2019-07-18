42d4c1ddf19a56cd8bc5edc3fe80a32879a8883d
rohitghai06 <erghai91@gmail.com>
2019-07-18 12:09:01 +0530

    Updates

---
+++ precache-manifest.36fb72ec02fa25fe2333d377b7cc337e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d315a2a4b8d43cc92d42511e841802a",
+    "url": "/StarWarsCodeFiles/index.html"
+  },
+  {
+    "revision": "5d1b09d2b2194e68bc96",
+    "url": "/StarWarsCodeFiles/static/css/main.9ea6d243.chunk.css"
+  },
+  {
+    "revision": "ba50d4be8917afc40e7e",
+    "url": "/StarWarsCodeFiles/static/js/2.e872fb4d.chunk.js"
+  },
+  {
+    "revision": "5d1b09d2b2194e68bc96",
+    "url": "/StarWarsCodeFiles/static/js/main.b1329f1b.chunk.js"
+  },
+  {
+    "revision": "5b9b7659043e595b065d",
+    "url": "/StarWarsCodeFiles/static/js/runtime~main.b62354c8.js"
+  }
+]);
